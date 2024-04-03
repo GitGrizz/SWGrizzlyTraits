91202e98bd15a51b630f6d569ddac98653708b73
GitGrizz <165746322+GitGrizz@users.noreply.github.com>
2024-04-02 23:16:45 -0400

    Trait Adjustments
    What is, for now, a personal project to change the weapon attributes in Spartan Weaponry to better fit my personal modpack

--- src/main/java/com/oblivioussp/spartanweaponry/data/ModWeaponTraitTagsProvider.java
+++ src/main/java/com/oblivioussp/spartanweaponry/data/ModWeaponTraitTagsProvider.java
@@ -29,27 +29,27 @@ public class ModWeaponTraitTagsProvider extends ForgeRegistryTagsProvider<Weapon
 	{
 		tag(ModWeaponTraitTags.DAGGER).add(WeaponTraits.THROWABLE.get(), WeaponTraits.DAMAGE_BONUS_BACKSTAB.get());
 		tag(ModWeaponTraitTags.PARRYING_DAGGER).add(WeaponTraits.BLOCK_MELEE.get());
-		tag(ModWeaponTraitTags.LONGSWORD).add(WeaponTraits.TWO_HANDED_1.get());
-		tag(ModWeaponTraitTags.KATANA).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.DAMAGE_BONUS_CHEST.get(), WeaponTraits.SWEEP_1.get());
+		tag(ModWeaponTraitTags.LONGSWORD).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.BLOCK_MELEE.get());
+		tag(ModWeaponTraitTags.KATANA).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.BLOCK_MELEE.get(), WeaponTraits.DAMAGE_BONUS_CHEST.get(), WeaponTraits.SWEEP_1.get());
 		tag(ModWeaponTraitTags.SABER).add(WeaponTraits.DAMAGE_ABSORB.get(), WeaponTraits.DAMAGE_BONUS_CHEST.get(), WeaponTraits.SWEEP_1.get());
 		tag(ModWeaponTraitTags.RAPIER).add(WeaponTraits.DAMAGE_ABSORB.get(), WeaponTraits.DAMAGE_BONUS_UNARMOURED.get());
-		tag(ModWeaponTraitTags.GREATSWORD).add(WeaponTraits.TWO_HANDED_2.get(), WeaponTraits.REACH_1.get(), WeaponTraits.SWEEP_3.get());
+		tag(ModWeaponTraitTags.GREATSWORD).add(WeaponTraits.TWO_HANDED_2.get(), WeaponTraits.BLOCK_MELEE.get(), WeaponTraits.REACH_1.get(), WeaponTraits.SWEEP_3.get());
 		tag(ModWeaponTraitTags.CLUB).add(WeaponTraits.NAUSEA.get());
 		tag(ModWeaponTraitTags.CESTUS).add(WeaponTraits.QUICK_STRIKE.get());
-		tag(ModWeaponTraitTags.BATTLE_HAMMER).add(WeaponTraits.KNOCKBACK.get(), WeaponTraits.NAUSEA.get(), WeaponTraits.HAMMER_SLAM.get());
-		tag(ModWeaponTraitTags.WARHAMMER).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.ARMOUR_PIERCING.get());
+		tag(ModWeaponTraitTags.BATTLE_HAMMER).add(WeaponTraits.KNOCKBACK.get(), WeaponTraits.NAUSEA.get(), WeaponTraits.ARMOUR_PIERCING.get(), WeaponTraits.HAMMER_SLAM.get());
+		tag(ModWeaponTraitTags.WARHAMMER).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.BLOCK_MELEE.get(), WeaponTraits.ARMOUR_PIERCING.get());
 		tag(ModWeaponTraitTags.SPEAR).add(WeaponTraits.REACH_1.get());
-		tag(ModWeaponTraitTags.HALBERD).add(WeaponTraits.TWO_HANDED_2.get(), WeaponTraits.REACH_1.get(), WeaponTraits.SHIELD_BREACH.get());
-		tag(ModWeaponTraitTags.PIKE).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.REACH_2.get());
+		tag(ModWeaponTraitTags.HALBERD).add(WeaponTraits.TWO_HANDED_2.get(), WeaponTraits.BLOCK_MELEE.get(), WeaponTraits.REACH_1.get(), WeaponTraits.SHIELD_BREACH.get());
+		tag(ModWeaponTraitTags.PIKE).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.BLOCK_MELEE.get(), WeaponTraits.REACH_2.get());
 		tag(ModWeaponTraitTags.LANCE).add(WeaponTraits.REACH_1.get(), WeaponTraits.DAMAGE_BONUS_RIDING.get(), WeaponTraits.SWEEP_1.get());
 		tag(ModWeaponTraitTags.THROWING_KNIFE).add(WeaponTraits.DAMAGE_BONUS_THROWN_1.get());
 		tag(ModWeaponTraitTags.TOMAHAWK).add(WeaponTraits.DAMAGE_BONUS_THROWN_1.get());
 		tag(ModWeaponTraitTags.JAVELIN).add(WeaponTraits.DAMAGE_BONUS_THROWN_2.get());
 		tag(ModWeaponTraitTags.BOOMERANG);
-		tag(ModWeaponTraitTags.BATTLEAXE).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.VERSATILE_AXE.get());
-		tag(ModWeaponTraitTags.FLANGED_MACE).add(WeaponTraits.DAMAGE_BONUS_UNDEAD.get());
+		tag(ModWeaponTraitTags.BATTLEAXE).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.BLOCK_MELEE.get(), WeaponTraits.VERSATILE_AXE.get());
+		tag(ModWeaponTraitTags.FLANGED_MACE).add(WeaponTraits.DAMAGE_BONUS_UNDEAD.get(), WeaponTraits.ARMOUR_PIERCING.get());
 		tag(ModWeaponTraitTags.GLAIVE).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.REACH_1.get(), WeaponTraits.SWEEP_2.get());
-		tag(ModWeaponTraitTags.QUARTERSTAFF).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.SWEEP_2.get());
+		tag(ModWeaponTraitTags.QUARTERSTAFF).add(WeaponTraits.TWO_HANDED_1.get(), WeaponTraits.BLOCK_MELEE.get(), WeaponTraits.SWEEP_2.get());
 		tag(ModWeaponTraitTags.SCYTHE).add(WeaponTraits.DAMAGE_BONUS_HEAD.get(), WeaponTraits.DECAPITATE.get());
 		
 		tag(ModWeaponTraitTags.WOOD);
